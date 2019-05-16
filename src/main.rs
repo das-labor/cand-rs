3e23ea040ad9dd33f64fd2ac021e9cf9e765cefb
fridtjof <2780577+fridtjof@users.noreply.github.com>
2019-05-16 16:55:28 +0200

    working CanTCPCodec

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,77 @@
 use std::env;
 
-use cand::can::can_uart::canu_init;
-use cand::can::can_tcp::*;
-use std::ffi::CString;
+extern crate tokio;
 
-static GREGS: [&str; 19] = [
-    "GS",
-    "FS",
-    "ES",
-    "DS",
-    "EDI",
-    "ESI",
-    "EBP",
-    "ESP",
-    "EBX",
-    "EDX",
-    "ECX",
-    "EAX",
-    "TRAPNO",
-    "ERR",
-    "EIP",
-    "CS",
-    "EFL",
-    "UESP",
-    "SS"
-];
+use tokio::net::TcpListener;
+use tokio::prelude::*;
 
-fn event_loop() {
+use tokio::codec::{Decoder, Encoder};
+use tokio::codec::Framed;
+use bytes::BytesMut;
+use bytes::BufMut;
+use tokio::io;
 
+const HEADER_LENGTH: usize = 2;
+const MAX_PAYLOAD_LENGTH: usize = 18;
+
+struct CanTCPCodec;
+
+#[derive(Debug)]
+struct CanTCPPacket {
+    cmd: u8,
+    data: Vec<u8>
+}
+
+impl CanTCPPacket {
+
+}
+
+impl Decoder for CanTCPCodec {
+    type Item = CanTCPPacket;
+    type Error = io::Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        if src.len() == 0 {
+            // why would we ever return any data for an empty input???
+            return Ok(None);
+        }
+
+        let payload_length = src[0] as usize;
+
+        if src.len() >= payload_length + HEADER_LENGTH
+        && payload_length <= MAX_PAYLOAD_LENGTH {
+            // we have enough bytes, split off a packet
+            let packet_data = src.split_to(HEADER_LENGTH + payload_length);
+
+            let slice = &packet_data[HEADER_LENGTH..payload_length];
+
+            let packet = CanTCPPacket {
+                cmd: packet_data[1],
+                data: slice.to_vec()
+            };
+
+            Ok(Some(packet))
+        }
+        else {
+            // more bytes are needed
+            Ok(None)
+        }
+    }
+}
+
+impl Encoder for CanTCPCodec {
+    type Item = CanTCPPacket;
+    type Error = io::Error;
+
+    fn encode(&mut self, item: Self::Item, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        dst.reserve(HEADER_LENGTH + item.data.len());
+
+        dst.put_u8(item.data.len() as u8);
+        dst.put_u8(item.cmd);
+        dst.put(item.data);
+
+        Ok(())
+    }
 }
 
 fn main() {
@@ -35,13 +79,21 @@ fn main() {
 
     let port = 2342;
 
-    let tmp = 0;
-    let raw_vid: [u8; 2] = [ 0xc0, 0x16 ];
-    let raw_pid: [u8; 2] = [ 0xdc, 0x05 ];
+    let addr = "127.0.0.1:2342".parse().unwrap();
+    let listener = TcpListener::bind(&addr).unwrap();
 
-    canu_init("/dev/tty.usbmodem46041");
+    let server = listener.incoming().for_each(|sock| {
+        println!("connection accepted");
+        let framed_sock = Framed::new(sock, CanTCPCodec {});
 
-    cann_listen(CString::new("2342").unwrap());
+        framed_sock.for_each(|frame| {
+            dbg!(frame);
+            Ok(())
+        })
+    })
+    .map_err(|err| {
+        println!("accept error = {:?}", err);
+    });
 
-    event_loop();
+    tokio::run(server);
 }
